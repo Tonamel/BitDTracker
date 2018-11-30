35848533d9a28320807d267976495e23f5572abe
Tonamel <ian@studiocypher.com>
2018-11-29 23:44:15 -0500

    Hooking Vue up to the app

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hey there, {{greeting}}!</h1>
+</template>
+
+<script>
+    module.exports = {
+        data: function(){
+            return {
+                greeting: "world"
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
